a6c38437287c26b954e77d6daaf213ffd40e3937
arsene1995 <shanlu.uiuc@gmail.com>
2025-12-22 19:12:03 -0600

    clean up source project

--- benchmarks/VeruSAGE-Bench/source-projects/anvil-controller/verified/verify.sh
+++
@@ -1,1 +0,0 @@
-~/programs/verus-202509/verus/source/target-verus/release/verus -L dependency=../deps_hack/target/debug/deps --extern=deps_hack="../deps_hack/target/debug/libdeps_hack.rlib" <rust file>
